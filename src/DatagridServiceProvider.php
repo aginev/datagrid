177e4576deae4efa74c3e414d0870815a9a1adfc
Atanas Ginev <atanas.ginev@gmail.com>
2016-01-24 13:58:49 +0200

    Registing Form and HTML facedes fixed to match laravelcollective/html

--- src/DatagridServiceProvider.php
+++ src/DatagridServiceProvider.php
@@ -1,41 +1,47 @@
 <?php namespace Aginev\Datagrid;
 
+use Collective\Html\FormFacade;
+use Collective\Html\HtmlFacade;
+use Collective\Html\HtmlServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
-class DatagridServiceProvider extends ServiceProvider {
+class DatagridServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-		// Tell Laravel where the views for a given namespace are located.
-		$this->loadViewsFrom(__DIR__ . '/Views', 'datagrid');
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Tell Laravel where the views for a given namespace are located.
+        $this->loadViewsFrom(__DIR__ . '/Views', 'datagrid');
 
-		$this->publishes([
-			__DIR__ . '/Views' => base_path('resources/views/vendor/datagrid'),
-			base_path('vendor/aginev/datagrid/src/Views') => base_path('resources/views/vendor/datagrid'),
-		], 'views');
-	}
+        $this->publishes([
+            __DIR__ . '/Views'                            => base_path('resources/views/vendor/datagrid'),
+            base_path('vendor/aginev/datagrid/src/Views') => base_path('resources/views/vendor/datagrid'),
+        ], 'views');
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register() {
-		// Register the HtmlServiceProvider
-		App::register('Illuminate\Html\HtmlServiceProvider');
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register the HtmlServiceProvider
+        App::register(HtmlServiceProvider::class);
 
-		// Add aliases to Form/Html Facade
-		$loader = AliasLoader::getInstance();
-		$loader->alias('Form', 'Illuminate\Html\FormFacade');
-		$loader->alias('HTML', 'Illuminate\Html\HtmlFacade');
+        // Add aliases to Form/Html Facade
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Form', FormFacade::class);
+        $loader->alias('HTML', HtmlFacade::class);
 
-		// Add alias for datagrid
-		$loader->alias('Datagrid', 'Aginev\Datagrid\Datagrid');
-	}
+        // Add alias for datagrid
+        $loader->alias('Datagrid', 'Aginev\Datagrid\Datagrid');
+    }
 }
