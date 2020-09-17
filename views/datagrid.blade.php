6d11e414dcaca990e5a0704694bca69da1867922
Atanas Ginev <atanas.ginev@gmail.com>
2020-09-17 15:19:47 +0300

    Removed html helpers

--- views/datagrid.blade.php
+++ views/datagrid.blade.php
@@ -1,13 +1,13 @@
 <div class="dg-wrapper" {!! $id ? 'id="'. $id .'"' : null !!}>
 
 	@if ($grid->hasFilters())
-		{!! Form::open(['role' => 'form', 'method' => 'GET']) !!}
+		<form action="" role="form" method="get">
 
-		{!! Form::hidden('f[order_by]', $grid->getFilter('order_by', '')); !!}
-		{!! Form::hidden('f[order_dir]', $grid->getFilter('order_dir', 'ASC')); !!}
+		<input type="hidden" name="f[order_by]" value="{{ $grid->getFilter('order_by', '') }}" />
+		<input type="hidden" name="f[order_dir]" value="{{ $grid->getFilter('order_dir', '') }}" />
 
 		@foreach ($grid->getHiddens() as $name => $value)
-			{!! Form::hidden($name, $value); !!}
+			<input type="hidden" name="{{ $name }}" value="{{ $value }}" />
 		@endforeach
 	@endif
 
@@ -23,7 +23,20 @@
 				@if ($col->isAction() === false)
 					<th data-dg-col="{{ $col->getKey() }}" {!! $col->getAttributesHtml() !!}>
 						@if ($col->isSortable())
-							<a href="{{ Datagrid::getCurrentRouteLink($grid->getSortParams($col->getKey())) }}">{!! $col->getTitle() !!}<i class="fa @if (\Request::input('f.order_by', '') == $col->getKey() && \Request::input('f.order_dir', 'ASC') == 'ASC') fa-sort-asc @elseif (\Request::input('f.order_by', '') == $col->getKey() && \Request::input('f.order_dir', 'ASC') == 'DESC') fa-sort-desc @else fa-sort @endif"></i></a>
+							<a
+								href="{{ \Aginev\Datagrid\Datagrid::getCurrentRouteLink($grid->getSortParams($col->getKey())) }}"
+								title="{{ __('Order by :title', ['title' => $col->getTitle()]) }}"
+							>
+								{!! $col->getTitle() !!}
+
+								@if (request()->input('f.order_by', '') == $col->getKey() && request()->input('f.order_dir', 'ASC') == 'ASC')
+									{!! config('datagrid.icons.asc') !!}
+								@elseif (request()->input('f.order_by', '') == $col->getKey() && request()->input('f.order_dir', 'ASC') == 'DESC')
+									{!! config('datagrid.icons.desc') !!}
+								@else
+									{!! config('datagrid.icons.sort', '') !!}
+								@endif
+							</a>
 						@else
 							{!! $col->getTitle() !!}
 						@endif
@@ -49,22 +62,28 @@
 						@if ($col->hasFilters())
 							<th data-dg-col="{{ $col->getKey() }}" {!! $col->getAttributesHtml() !!}>
 								@if ( is_array($col->getFilters()) && count($col->getFilters()) > 0 )
-									{!!
-										Form::select(
-											$col->getFilterName(),
-											$col->getFilters(true),
-											$grid->getFilter($col->getKey()),
-											($col->hasFilterMany() ? ['multiple' => 'multiple'] : []) + array('class' => 'form-control input-sm', 'data-dg-type' => "filter")
-										)
-									!!}
+									<select
+										name="{{ $col->getFilterName() }}"
+										class="form-control input-sm"
+										data-dg-type="filter"
+										{{ $col->hasFilterMany() ? 'multiple' : null }}
+									>
+										@foreach($col->getFilters(true) as $value => $title)
+											<option
+												value="{{ $value }}"
+												{{ $grid->getFilter($col->getKey()) == $value ? 'selected' : null }}
+											>{{ $title }}</option>
+										@endforeach
+									</select>
 								@else
-									{!!
-										Form::text(
-											$col->getFilterName(),
-											$grid->getFilter($col->getKey()),
-											array('class' => 'form-control input-sm', 'data-dg-type' => "filter", 'placeholder' => $col->getTitle())
-										)
-									!!}
+									<input
+										type="text"
+										name="{{ $col->getFilterName() }}"
+										value="{{ $grid->getFilter($col->getKey()) }}"
+										class="form-control input-sm"
+										data-dg-type="filter"
+										placeholder="{{ $col->getTitle() }}"
+									>
 								@endif
 							</th>
 						@else
@@ -72,8 +91,15 @@
 						@endif
 					@else
 						<th data-dg-col="actions">
-							<button class="btn btn-success btn-sm" type="submit" title="Search..."><i class="fa fa-search" aria-hidden="true"></i></button>
-							<a href="{{ \Request::url() }}" class="btn btn-danger btn-sm" title="Clear filters"><i class="fa fa-remove" aria-hidden="true"></i></a>
+							<button
+								class="btn btn-success btn-sm"
+								type="submit"
+								title="{{ __('Search') }}"
+							>{!! config('datagrid.icons.search') !!}</button>
+							<a href="{{ request()->url() }}"
+								 class="btn btn-danger btn-sm"
+								 title="{{ __('Clear') }}"
+							>{!! config('datagrid.icons.clear') !!}</a>
 						</th>
 					@endif
 				@endforeach
@@ -86,7 +112,13 @@
 		@forelse($grid->getRows() as $row)
 			<tr data-dg-type="data-row">
 				@if ($grid->isItBulkable())
-					<th data-dg-col="bulks"><input type="checkbox" value="{{ $row->{$grid->getBulk()} }}" data-dg-bulk-select="row" /></th>
+					<th data-dg-col="bulks">
+						<input
+							type="checkbox"
+							value="{{ $row->{$grid->getBulk()} }}"
+							data-dg-bulk-select="row"
+						/>
+					</th>
 				@endif
 
 				@foreach ($grid->getColumns() as $col)
@@ -110,7 +142,7 @@
 	</table>
 
 	@if ($grid->hasFilters())
-		{!! Form::close() !!}
+		</form>
 	@endif
 
 	@if ($grid->hasPagination())
